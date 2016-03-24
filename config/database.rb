922e14f282932b9f807afd521e14ecbad6949c36
Osama Khan <osamakhn@gmail.com>
2016-03-24 01:49:40 +0000

    integrate postgres to skeleton instead of sqlite

--- config/database.rb
+++ config/database.rb
@@ -4,10 +4,10 @@ configure do
     ActiveRecord::Base.logger = Logger.new(STDOUT)
   end
 
-  set :database, {
-    adapter: "sqlite3",
-    database: "db/db.sqlite3"
-  }
+  # set :database, {
+  #   adapter: "sqlite3",
+  #   database: "db/db.sqlite3"
+  # }
 
   # Load all models from app/models, using autoload instead of require
   # See http://www.rubyinside.com/ruby-techniques-revealed-autoload-1652.html
@@ -15,5 +15,36 @@ configure do
     filename = File.basename(model_file).gsub('.rb', '')
     autoload ActiveSupport::Inflector.camelize(filename), model_file
   end
+
+configure :development do
+ 
+ ActiveRecord::Base.establish_connection(
+  adapter: 'postgresql',
+  database: 'app_vakanz',
+  username: 'development',
+  password: 'development',
+  host: 'localhost',
+  port: 5432,
+  pool: 5,
+  encoding: 'unicode',
+  min_messages: 'error',
+  encoding: 'utf8'
+   )
+ set :show_exceptions, true
+ 
+end
+
+# configure :production do
+#  db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+
+#  ActiveRecord::Base.establish_connection(
+#    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+#    :host     => db.host,
+#    :username => db.user,
+#    :password => db.password,
+#    :database => db.path[1..-1],
+#    :encoding => 'utf8'
+#  )
+# end
 
 end
